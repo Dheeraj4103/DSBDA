2e07370dcad1b2377db8e9f4a5fc17a2c4f5605c
J_Dheeraj@4103 <69622080+Dheeraj4103@users.noreply.github.com>
2023-03-24 18:07:17 +0000

    updated LogMain

--- Assignment2/src/maxLog/LogMain.java
+++ Assignment2/src/maxLog/LogMain.java
@@ -26,6 +26,35 @@ public class LogMain {
 		FileInputFormat.addInputPath(job, new Path(args[0]));
 		FileOutputFormat.setOutputPath(job, new Path(args[1]));
 		
-		System.exit(job.waitForCompletion(true) ? 0 : 1);
+		try {
+			FileSystem fs = FileSystem.get(URI.create("hdfs://localhost:9000" + args[1]), conf);
+			String fname = fs.listStatus(new Path(args[1]))[1].getPath().toString();
+			FSDataInputStream in = null;
+			in = fs.open(new Path(fname));
+//            File Obj = new File("/home/hduser/Documents/Assignment2/part-r-00000");
+            Scanner Reader = new Scanner(in);
+            String key = "";
+            int max = 0;
+            
+            while (Reader.hasNextLine()) {
+                String data = Reader.nextLine();
+                String[] vals = data.split("	");
+//                System.out.println(Arrays.toString(vals));
+                int count = Integer.parseInt(vals[1]);
+                if (count > max) {
+                	key = vals[0];
+                	max = count;
+                }
+            }
+            System.out.println("-----------------------------------------");
+            System.out.println("IP address used maximum no of times:- ");
+            System.out.println(key + " -> " + max);
+            System.out.println("-----------------------------------------");
+            Reader.close();
+        }
+        catch (FileNotFoundException e) {
+            System.out.println("An error has occurred.");
+            e.printStackTrace();
+        }
 	}
 }
